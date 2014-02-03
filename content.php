a7e37955da879f960ff11a0083e1b72b40c3e7dd
Sebastien <sebastien@sebs-studio.com>
2014-02-03 15:42:30 +0100

    Corrected HTML markup in title

--- content.php
+++ content.php
@@ -13,9 +13,9 @@
 	<?php highwind_content_header_top(); ?>
 
 	<?php if ( is_single() ) : ?>
-		<h1 class="post-title" data-text="<?php the_title(); ?>"><?php the_title(); ?></h1>
+		<h1 class="post-title" data-text="<?php the_title_attribute(); ?>"><?php the_title(); ?></h1>
 	<?php else : ?>
-		<h1 class="post-title" data-text="<?php the_title(); ?>"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'highwind' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h1 class="post-title" data-text="<?php the_title_attribute(); ?>"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'highwind' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	<?php endif; ?>
 
 	<?php highwind_content_header_bottom(); ?>

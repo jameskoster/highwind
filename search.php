0b0f4db07a6a0382d731f1f8b5e4bd4e1e3879af
Coen Jacobs <coenjacobs@gmail.com>
2014-05-15 10:46:31 +0200

    Search template should use loop
    In old situation it's a 'custom' loop where it doesn't show things like
    post meta. By just loading the loop, this is consistent with other
    pages.

--- search.php
+++ search.php
@@ -27,11 +27,7 @@ get_header();
 
 		<?php /* Start the Loop */ ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'content', get_post_format() ); ?>
-
-		<?php endwhile; ?>
+		<?php get_template_part( 'loop' ); ?>
 
 	<?php else : ?>
 
